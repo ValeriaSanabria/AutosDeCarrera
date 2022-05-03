aafe3baeb2b5961895ee13128c3bcf987f95cb0e
Valeria Sanabria <103613127+ValeriaSanabria@users.noreply.github.com>
2022-05-03 06:40:45 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+let vuelta1: number = dato1.value;
+let vuelta2: number = dato2.value;
+let vuelta3: number = dato3.value;
+let vuelta4: number = dato4.value;
+
+let tiempoTotal = vuelta1 + vuelta2 + vuelta3 + vuelta4;
+console.log("Tiempo total:", tiempoTotal);
+console.log("Promedio de vuelta:", tiempoTotal / 4);
